df7fe4dbb602ce36114356ce39e5cec9a978911c
MuzaffarOrtiqov <muzaffarmike2@gmail.com>
2025-12-14 10:59:27 +0000

    Initial commit

---
+++ api.vybe/src/main/java/api/vybe/VybeApplication.java
@@ -0,0 +1,13 @@
+package api.vybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VybeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VybeApplication.class, args);
+	}
+
+}
---
+++ api.vybe/src/test/java/api/vybe/VybeApplicationTests.java
@@ -0,0 +1,13 @@
+package api.vybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VybeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
