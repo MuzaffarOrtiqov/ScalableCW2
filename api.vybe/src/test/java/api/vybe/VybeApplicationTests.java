535e08c53700977edb2055b8edc80d72c37d9177
MuzaffarOrtiqov <muzaffarmike2@gmail.com>
2026-01-13 11:09:45 +0000

    new file chnages on test folder

--- api.vybe/src/test/java/api/vybe/VybeApplicationTests.java
+++ api.vybe/src/test/java/api/vybe/VybeApplicationTests.java
@@ -2,8 +2,10 @@ package api.vybe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("ci")
 class VybeApplicationTests {
 
 	@Test
